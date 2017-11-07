bf78e0659386743f03c0f851d7b500fc73701e12
Steffan Halvorsen <steffanhalv@gmail.com>
2017-11-07 16:46:05 +0100

    Updated icon

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import { app, BrowserWindow } from 'electron'
+import path from 'path'
 
 /**
  * Set `__static` path to static files in production
@@ -22,7 +23,8 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     height: 563,
     useContentSize: true,
-    width: 1000
+    width: 1000,
+    icon: path.join(__dirname, 'build/icons/256x256.png')
   })
 
   mainWindow.loadURL(winURL)
