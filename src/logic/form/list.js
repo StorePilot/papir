9871d8eb4e6201eae10d39f4c35f996177d7e009
Steffan Halvorsen <steffanhalv@gmail.com>
2017-12-22 01:49:22 +0100

    Made lists accept Endpoint constructors to

--- src/logic/form/list.js
+++ src/logic/form/list.js
@@ -4,14 +4,24 @@ import Endpoint from './endpoint'
  * List
  */
 export default class List extends Endpoint {
-  constructor (endpoint, controller = null, apiSlug = null, predefined = {}, config = {}) {
+  constructor (Endpoint, controller = null, apiSlug = null, predefined = {}, config = {}) {
     /**
      * If no controller defined, create one from endpoint if it is not a string
      */
-    if (controller === null && endpoint.constructor === Object) {
-      controller = endpoint.shared.controller
-    } else if (controller === null) {
-      console.error('No controller defined for List', endpoint)
+    let endpoint
+    if (controller !== null) {
+      // Check if Endpoint is constructor and try to resolve
+      try {
+        endpoint = new Endpoint(controller, apiSlug, predefined, config)
+      } catch (e) {
+        // Controller was not a constructor
+      }
+    } else if (controller === null && Endpoint.constructor === Object) {
+      // If controller is not set, assume endpoint already is a constructed Endpoint object
+      endpoint = Endpoint
+      controller = endpoint.shared.controller // Get the controller from endpoint
+    } else {
+      console.error('No controller defined for List', Endpoint)
     }
     /**
      * Pass to Endpoint model controller, id = null, apiSlug = controller.default, predefined = {}
